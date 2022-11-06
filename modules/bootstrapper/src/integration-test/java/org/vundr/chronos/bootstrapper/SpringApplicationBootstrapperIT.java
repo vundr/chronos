50abd9a9781df452ff204fec16d52a52bfcaaf81
Andrey Vinel <vinel.work03@gmail.com>
2022-11-06 21:37:24 +0100

    Tests

---
+++ modules/bootstrapper/src/integration-test/java/org/vundr/chronos/bootstrapper/SpringApplicationBootstrapperIT.java
@@ -0,0 +1,14 @@
+package org.vundr.chronos.bootstrapper;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Tag("integrationtest")
+class SpringApplicationBootstrapperIT {
+
+    @Test
+    void contextIsStarted() {
+    }
+}
---
+++ modules/bootstrapper/src/test/java/org/vundr/chronos/bootstrapper/SpringApplicationBootstrapperTest.java
@@ -0,0 +1,12 @@
+package org.vundr.chronos.bootstrapper;
+
+import org.junit.jupiter.api.Test;
+
+
+class SpringApplicationBootstrapperTest {
+
+    @Test
+    void test() {
+
+    }
+}
