5e6696f11fd96f59122f989a841979a49cc75457
Andrey Vinel <vinel.work03@gmail.com>
2022-11-07 09:42:59 +0100

    Tests

--- modules/bootstrapper/src/integration-test/java/org/vundr/chronos/bootstrapper/SpringApplicationBootstrapperIT.java
+++ modules/bootstrapper/src/integration-test/java/org/vundr/chronos/bootstrapper/SpringApplicationBootstrapperIT.java
@@ -1,11 +1,9 @@
 package org.vundr.chronos.bootstrapper;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Tag("integrationtest")
 class SpringApplicationBootstrapperIT {
 
     @Test
