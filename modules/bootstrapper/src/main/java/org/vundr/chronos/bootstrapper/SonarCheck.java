d0478dbd4b8c85e29858f75c4efaaf8f631c49ae
Andrey Vinel <vinel.work03@gmail.com>
2022-11-07 11:26:15 +0100

    Tests

--- modules/bootstrapper/src/main/java/org/vundr/chronos/bootstrapper/SonarCheck.java
+++ modules/bootstrapper/src/main/java/org/vundr/chronos/bootstrapper/SonarCheck.java
@@ -5,4 +5,5 @@ public class SonarCheck {
     public String getString() {
         return "string";
     }
+
 }
---
+++ modules/bootstrapper/src/main/java/org/vundr/chronos/bootstrapper/SonarItCheck.java
@@ -0,0 +1,11 @@
+package org.vundr.chronos.bootstrapper;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SonarItCheck {
+
+    public String test() {
+        return "string";
+    }
+}
